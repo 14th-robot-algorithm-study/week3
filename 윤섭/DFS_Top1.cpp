6feafcee2fd9978e4cdb44d60e1bee79d5b476f4
choiyunseop <xlrkfmtptm@naver.com>
2025-08-08 15:30:31 +0900

    [DFS의 탑 1번 ] 네트워크 바이러스
    [DFS의 탑 1번 ] 네트워크 바이러스
    https://pro.mincoding.co.kr/problem-step/7/level/79/detail/2357

--- 윤섭/DFS_Top1.cpp
+++ 윤섭/DFS_Top1.cpp
@@ -4,16 +4,17 @@ using namespace std;
 
 int nodeCnt, edgeCnt;
 vector<int> v[101];
-int visited[101]; 
+int visited[100];
 int totalCnt = 0;
 
 void func(int now) {
 	visited[now] = 1;
+
 	for (int i = 0; i < v[now].size(); i++) {
-		int next = v[now][i];
-		if (visited[next] == 0) {
-			totalCnt++;       
-			func(next);       
+		if (visited[v[now][i]] == 0) {
+			visited[v[now][i]] = 1;
+			totalCnt++;
+			func(v[now][i]);
 		}
 	}
 }
@@ -29,7 +30,8 @@ int main() {
 		v[to].push_back(from);
 	}
 
-	func(1);  
+	visited[1] = 1;
+	func(1);
 
 	cout << totalCnt << '\n';
 	return 0;
