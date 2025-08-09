92c88253f47d9d8837511f16a39baf4e3e60d09a
Eunbyul <eunbyul0805@gmail.com>
2025-08-09 23:35:56 +0900

    [Level 19-1] Direct 사용하기

---
+++ 은지/Lev19_1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+struct Point {
+	int x, y;
+};
+
+
+int main() {
+	int dy[4] = { -1, 1, 0, 0 };
+	int dx[4] = { 0, 0, -1, 1 };
+
+	int arr[3][3] = {
+		{3, 5, 4},
+		{1, 1, 2},
+		{1, 3, 9}
+	};
+
+	Point point;
+	std::cin >> point.y >> point.x;
+
+	int sum = 0;
+
+	for (int i = 0; i < 4; i++) {
+		int ny = point.y + dy[i];
+		int nx = point.x + dx[i];
+
+		if (nx < 0 || ny < 0 || ny >= 3 || nx >= 3) {
+			continue;
+		}
+
+		sum += arr[ny][nx];
+	}
+
+	std::cout << sum;
+
+	return 0;
+}
