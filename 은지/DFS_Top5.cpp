1099c10c916c9572ba413447a00f84487cd2975a
Eunbyul <eunbyul0805@gmail.com>
2025-08-10 19:54:35 +0900

    [DFS의 탑 5번] 해밀턴 회로

---
+++ 은지/DFS_Top5.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+
+int N;
+int min_val = 21e8;
+int arr[100][100];
+int visited[100];
+
+
+void func(int lev, int count, int cnt) {
+	if (cnt == N) {
+		count += arr[lev][0];
+		min_val = std::min(min_val, count);
+		return;
+	}
+
+
+	for (int i = 0; i < N; i++) {
+		if (visited[i] == 1) {
+			continue;
+		}
+
+		if (arr[lev][i] == 0) {
+			continue;
+		}
+
+		visited[i] = 1;
+		func(i, count + arr[lev][i], cnt + 1);
+		visited[i] = 0;
+	}
+}
+
+
+int main() {
+
+	std::cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			std::cin >> arr[i][j];
+		}
+	}
+
+	visited[0] = 1;
+	func(0, 0, 1);
+
+	std::cout << min_val;
+	return 0;
+}
