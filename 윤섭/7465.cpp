b32d50ebe4d1b62d213dbefc5c68ffc777454f44
choiyunseop <xlrkfmtptm@naver.com>
2025-09-04 09:31:15 +0900

    [7465] 창용 마을 무리의 개수
    [7465] 창용 마을 무리의 개수
    https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWngfZVa9XwDFAQU

--- 윤섭/7465.cpp
+++ 윤섭/7465.cpp
@@ -1,3 +1,4 @@
+/* union find */
 #include <iostream>
 #include <cstring>
 using namespace std;
@@ -39,6 +40,66 @@ int main() {
 
 		for (int i = 1; i <= N; i++) {
 			if (parent[i] == i) group_cnt++;
+		}
+
+		cout << "#" << tc << " " << group_cnt << "\n";
+	}
+}
+
+/* BFS */
+#include <iostream>
+#include <queue>
+#include <cstring>
+using namespace std;
+
+int T;
+int N, M;
+int group_cnt;
+vector<vector<int>> v;
+int visited[101];
+
+void bfs(int st) {
+	visited[st] = 1;
+	group_cnt++;
+	queue <int> q;
+	q.push(st);
+
+	while (!q.empty()) {
+		int cur = q.front();
+		q.pop();
+
+		for (int i = 0; i < v[cur].size(); i++) {
+			int next = v[cur][i];
+			if (visited[next] == 1) continue;
+
+			visited[next] = 1;
+			q.push(next);
+		}
+	}
+}
+
+int main() {
+	cin >> T;
+	
+	for (int tc = 1; tc <= T; tc++) {
+		group_cnt = 0;
+		
+		cin >> N >> M;
+		v.assign(N + 1, vector<int>());
+		memset(visited, 0, sizeof(visited));
+
+		for (int i = 0; i < M; i++) {
+			int from, to;
+			cin >> from >> to;
+			v[from].push_back(to);
+			v[to].push_back(from);
+
+		}
+		
+		for (int i = 1; i <= N; i++) {
+			if (visited[i] == 0) {
+				bfs(i);
+			}
 		}
 
 		cout << "#" << tc << " " << group_cnt << "\n";
